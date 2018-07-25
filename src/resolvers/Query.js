ce91129eaa1ee35ebec85159ade87a13fef524ab
Zachary Haynie <zacharyrhaynie@gmail.com>
2018-07-25 11:33:01 -0600

    First Commit - Got datamodel and seed data
    Starting to work on schema.graphql and the resolvers. Wanted to push it up to show how progression has been

---
+++ src/resolvers/Query.js
@@ -0,0 +1,31 @@
+const { getUserId } = require('../utils')
+
+const Query = {
+  feed(parent, args, ctx, info) {
+    return ctx.db.query.posts({ where: { isPublished: true } }, info)
+  },
+
+  drafts(parent, args, ctx, info) {
+    const id = getUserId(ctx)
+
+    const where = {
+      isPublished: false,
+      author: {
+        id
+      }
+    }
+
+    return ctx.db.query.posts({ where }, info)
+  },
+
+  budget(parent, { id }, ctx, info) {
+    return ctx.db.query.budget({ where: { id } }, info)
+  },
+
+  me(parent, args, ctx, info) {
+    const id = getUserId(ctx)
+    return ctx.db.query.user({ where: { id } }, info)
+  },
+}
+
+module.exports = { Query }
